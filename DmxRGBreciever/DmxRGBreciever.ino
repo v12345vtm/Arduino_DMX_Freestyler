f271636a5f8b7bb1e1fb83265330857b607a9ad1
v12345vtm <plekke@yahoo.com>
2020-12-31 10:30:16 +0100

    Add files via upload

--- DmxRGBreciever/DmxRGBreciever.ino
+++ DmxRGBreciever/DmxRGBreciever.ino
@@ -3,10 +3,10 @@
 // address 1 (red) -> PWM Port 5
 // address 2 (green) -> PWM Port 6
 // address 3 (blue) -> PWM Port 9
-// 
+//
 // Copyright (c) 2011-2015 by Matthias Hertel, http://www.mathertel.de
 // This work is licensed under a BSD style license. See http://www.mathertel.de/License.aspx
-// 
+//
 // Documentation and samples are available at http://www.mathertel.de/Arduino
 // 25.07.2011 creation of the DmxSerial library.
 // 10.09.2011 fully control the serial hardware register
@@ -38,14 +38,12 @@ void setup () {
 
 
   DMXSerial.init(DMXReceiver);
-  
-  // set some default values
-   
 
-    DMXSerial.write(1, 0);
-   DMXSerial.write(2, 0);
-    DMXSerial.write(3, 255);
-  delay(500);
+  // set some default values
+
+
+
+
   // enable pwm outputs
   pinMode(RedPin,   OUTPUT); // sets the digital pin5 as output pwm led A
   pinMode(GreenPin, OUTPUT);//sets the digital pin6 as output
@@ -54,10 +52,10 @@ void setup () {
   pinMode(Redled,   OUTPUT); // sets the digital pin5 as output pwm led B
   pinMode(Greenled, OUTPUT);//sets the digital pin6 as output
   pinMode(Blueled,  OUTPUT);//sets the digital pin9 as output
-  
 
-//enable optional dipswitch to set differant dmx address then 1
-  pinMode(A0,  INPUT_PULLUP); 
+
+  //enable optional dipswitch to set differant dmx address then 1
+  pinMode(A0,  INPUT_PULLUP);
   pinMode(A1,  INPUT_PULLUP);
   pinMode(A2,  INPUT_PULLUP);
   pinMode(A3,  INPUT_PULLUP);
@@ -75,37 +73,57 @@ void setup () {
   bitWrite(DMXchannel, 5, !digitalRead(A5)) ;
   bitWrite(DMXchannel, 6, pinStateA6) ;
   bitWrite(DMXchannel, 7, pinStateA7) ;
-DMXchannel < 1 ? DMXchannel=1 : 0 ; //indien DIP switch dmx address is 0 , then set autoadres op 1
+  DMXchannel < 1 ? DMXchannel = 1 : 0 ; //indien DIP switch dmx address is 0 , then set autoadres op 1
 
-//uncomment to set DMX address  hardcoded
-  // DMXchannel  =  4;            
+  //uncomment to set DMX address  hardcoded
+  // DMXchannel  =  4;
+
+
+
+
+//  DMXSerial.write(1, 255);
+//  DMXSerial.write(2, 0);
+//  DMXSerial.write(3, 0);
+//  delay(500);
+//  DMXSerial.write(1, 0);
+//  DMXSerial.write(2, 255);
+//  DMXSerial.write(3, 0);
+//  delay(500);
+//  DMXSerial.write(1, 0);
+//  DMXSerial.write(2, 0);
+//  DMXSerial.write(3, 255);
+//  delay(500);
+//  //    DMXSerial.write(1, 0);
+//  // DMXSerial.write(2, 0);
+//  //  DMXSerial.write(3, 0);
+//  delay(500);
 }
 
 
 void loop() {
   // Calculate how long no data backet was received
   unsigned long lastPacket = DMXSerial.noDataSince();
-  
+
   if (lastPacket < 1500) {
-    // read recent DMX values and set pwm levels 
-    analogWrite(RedPin,   DMXSerial.read(DMXchannel +0 ));//channel 4 5 6 address of dmx universe
-    analogWrite(GreenPin, DMXSerial.read(DMXchannel +1));
-    analogWrite(BluePin,  DMXSerial.read(DMXchannel +2));
-    
-    analogWrite(Redled,  DMXSerial.read(DMXchannel +3));
-    analogWrite(Greenled,  DMXSerial.read(DMXchannel +4));
-    analogWrite(Blueled,  DMXSerial.read(DMXchannel +5));
+    // read recent DMX values and set pwm levels
+    analogWrite(RedPin,   DMXSerial.read(DMXchannel + 0 )); //channel 4 5 6 address of dmx universe
+    analogWrite(GreenPin, DMXSerial.read(DMXchannel + 1));
+    analogWrite(BluePin,  DMXSerial.read(DMXchannel + 2));
+
+    analogWrite(Redled,  DMXSerial.read(DMXchannel + 3));
+    analogWrite(Greenled,  DMXSerial.read(DMXchannel + 4));
+    analogWrite(Blueled,  DMXSerial.read(DMXchannel + 5));
 
   } else {
     // Show pure red color, when no data was received since 5 seconds or more.
- 
+
     analogWrite(RedPin,   RedDefaultLevel);
     analogWrite(GreenPin, GreenDefaultLevel);
     analogWrite(BluePin,  BlueDefaultLevel);
-    
-     analogWrite(Redled,  255);
-      analogWrite(Greenled,  0);
-       analogWrite(Blueled,  0);
+
+    analogWrite(Redled,  255);
+    analogWrite(Greenled,  0);
+    analogWrite(Blueled,  0);
   } // if
 }
 
